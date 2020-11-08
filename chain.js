e6ab3cc692427e89513523f5926b9907a4696b29
Your Name <you@example.com>
2020-11-08 13:55:44 +0530

    mod

---
+++ chain.js
@@ -0,0 +1,15 @@
+class chain{
+    constructor(bodyA,bodyB){
+        var option = {
+            bodyA:bodyA,bodyB:bodyB,stiffness:0.04,length:10
+        }
+        this.chain = Constraint.create(option)
+        World.add(world,this.chain)
+        
+    }
+    display(){
+      var pointA = this.chain.bodyA.position
+      var pointB = this.chain.bodyB.position
+      line(pointA.x,pointA.y,pointB.x,pointB.y) 
+    }
+}
